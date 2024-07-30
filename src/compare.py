cf6a6268dcaf5c863bfaaa0a12bc84fe369d4cdc
Sergey <sergey.tikin94@gmail.com>
2024-07-30 21:07:14 +0300

    fix

--- src/compare.py
+++ src/compare.py
@@ -4,31 +4,40 @@ import json
 
 class LoadVacancy(ABC):
     @abstractmethod
-    def read_file(self, file_name):
+    def read_file(self):
         pass
 
     @abstractmethod
-    def save_file(self, data, file_name):
+    def save_file(self, data):
         pass
 
     @abstractmethod
-    def del_file(self, file_name):
+    def del_file(self):
         pass
 
 
 class CreateFile(LoadVacancy):
-    def read_file(self, file_name):
-        with open(f"data/{file_name}", "r", encoding="utf8") as file:
+    def __init__(self):
+        self.__file_name = 'vacancy.json'
+
+    def read_file(self):
+        with open(f"data/{self.__file_name}", "r", encoding="utf8") as file:
             return json.load(file)
 
-    def save_file(self, data, file_name):
-        with open(f"data/{file_name}", "w", encoding="utf8") as file:
-            json.dump(data, file, ensure_ascii=False, indent=4)
+    def save_file(self, data):
+        vacancy = []
+        try:
+            vacancy.extend(self.read_file())
+        except json.decoder.JSONDecodeError as err:
+            pass
+        vacancy.extend(data)
+        with open(f"data/{self.__file_name}", "w", encoding="utf8") as file:
+            json.dump(vacancy, file, ensure_ascii=False, indent=4)
 
-    def get_data(self, criterion, file_name):
+    def get_data(self, criterion):
         """Метод получения данных из файла по указанным критериям"""
         criterion_vac = []
-        with open(f"data/{file_name}", "r", encoding="utf8") as file:
+        with open(f"data/{self.__file_name}", "r", encoding="utf8") as file:
             vacancies = json.load(file)
             for vac in vacancies:
                 if not vac["snippet"]["requirement"]:
@@ -38,6 +47,6 @@ class CreateFile(LoadVacancy):
                         criterion_vac.append(vac)
         return criterion_vac
 
-    def del_file(self, file_name):
-        with open(f"data/{file_name}", "w") as file:
+    def del_file(self):
+        with open(f"data/{self.__file_name}", "w") as file:
             pass
